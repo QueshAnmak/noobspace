845a39ead3c831a30a6392507746e344d81e7992
Alpha <samriddhjn@gmail.com>
2021-05-22 01:51:25 +0530

    fixed cursor blurring

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-document.addEventListener("keydown", function(){document.getElementById("laggaye").focus()});
+let something = false;
 
-function enter(event){
+document.addEventListener("keydown", function(event){
+    if (event.key != 'Backspace' && event.key != 'Delete'){
+    search.focus();
+    }
+    if (search.value.trim().length == 1 && something && event.key == 'Backspace'){
+        console.log("blurring");
+        search.blur();
+        search.value = '';
+        something = false;
+    }
+});
+
+function checkIfEnter(event){
 
     console.log(event.key);
+    const toSearch = search.value;
+
+    if (toSearch.trim().length > 0){
+        something = true;
+    }
 
     if (event.key === "Enter"){
 
-        const string1 = document.getElementsByClassName("search").item(0).value;    
-        console.log(string1);
-        window.open(`https://www.google.com/search?q=${string1}`, "_self");
+        // check if toSearch is url
+        urlPattern = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+        const toSearchIsUrl = toSearch.match(urlPattern);
+        
+        // if url, open directly
+        if (toSearchIsUrl)
+            open(toSearch, "_self");
+
+        // else google search
+        else
+            open(`https://www.google.com/search?q=${toSearch}`, "_self");
     }    
 }

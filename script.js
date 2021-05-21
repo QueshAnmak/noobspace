cf952be7629090fe3d71d633b6c8114a6e148112
Alpha <samriddhjn@gmail.com>
2021-05-21 22:07:01 +0530

    no need to mouse click now

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+document.addEventListener("keydown", function(){document.getElementById("laggaye").focus()});
+
 function enter(event){
+
     console.log(event.key);
 
     if (event.key === "Enter"){
 
-    const string1 = document.getElementsByClassName("search").item(0).value;
-    
-    console.log(string1);
-    window.open(`https://www.google.com/search?q=${string1}`);
+        const string1 = document.getElementsByClassName("search").item(0).value;    
+        console.log(string1);
+        window.open(`https://www.google.com/search?q=${string1}`, "_self");
     }    
 }
